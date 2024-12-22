efb74f5f1de8b68c1e5553e87b767c8eb6331187
vanlakhan <chanthakhaninsyxiengmai@gmail.com>
2024-12-22 19:32:34 +0700

    redeploy with build

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VANLAKHAN_PORTFOLIO/',
+  base: '/vanlakhan-portfolio/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
